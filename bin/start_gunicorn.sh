664bf51ca255716819557598511c2a8e87a96061
KeSHaMI <pasha.mukovoz@gmail.com>
2020-07-16 09:42:29 +0300

    heroku update

--- bin/start_gunicorn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /home/ubuntu/rest-kaffka/Kaffka-REST/env/bin/activate
-exec gunicorn -c "/home/ubuntu/rest-kaffka/Kaffka-REST/gunicorn_config.py" config.wsgi
-
-
-
-
--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-command = 'home/ubuntu/rest-kaffka/Kaffka-REST/env/bin/gunicorn'
-pythonpath = '/home/ubuntu/rest-kaffka/Kaffka-REST/config'
-bind = '127.0.0.1:8000'
-worker = 3
-user = 'ubuntu'
-limir_request_fields = 32000
-limit_request_field_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=config.settings'
