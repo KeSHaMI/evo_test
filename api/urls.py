c74c059662ed3db26067253b250f0b70a2e536a6
KeSHaMI <pasha.mukovoz@gmail.com>
2020-07-16 14:19:47 +0300

    File page button

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path('get_all/', get_all),
     path('get/<str:pk>', get),
     path('create/', create),
-    path('get_view', get_view)
+    path('get_view/<str: pk>', get_view)
 
 ]
 
